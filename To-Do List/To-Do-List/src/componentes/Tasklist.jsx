81ca1c763778e2db95b2bc80bb509d7c76ca58b4
Quiique_m <enriqueminguetcobos1@gmail.com>
2025-11-11 11:49:05 +0100

    v3: Ahora tengo una práctica de ToDoList

---
+++ To-Do List/To-Do-List/src/componentes/Tasklist.jsx
@@ -0,0 +1,8 @@
+  <div style ={{ textAlign: 'center'}}>
+    <h1>Lista de tareas pendientes</h1>
+    <form>
+      <input type="text" placeholder='Escribe la tarea aquí...' style={{width: "300px", height: "40px"}}></input>
+      <button type="submit" style={{  backgroundColor: "#4CAF50", color: "white", 
+        marginLeft: "20px", fontSize: "16px"}}>Enviar</button>
+    </form>
+  </div>
